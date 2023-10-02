b6dcd60e01cc0d18d086521a963bfea3995c8827
NahuelRoman1990 <nahuel.90.roman@gmail.com>
2023-10-02 20:28:43 -0300

    UltimoA

---
+++ src/accesoDatos/Conexion.java
@@ -0,0 +1,37 @@
+package accesoDatos;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author roman
+ */
+public class Conexion {
+    
+    private static final String URL = "jdbc:mariadb://localhost/";
+    private static final String DB = "comprasgrupo14";
+    private static final String USUARIO = "root";
+    private static final String PASSWORD = "";
+    private static Connection connection;
+
+    public Conexion() {
+    }
+
+    public static Connection getConnection() {
+        if (connection == null) {
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                connection = DriverManager.getConnection(URL + DB, USUARIO, PASSWORD);
+
+            } catch (ClassNotFoundException ex) {
+                JOptionPane.showMessageDialog(null, "Error al cargar drivers de coneexion");
+            } catch (SQLException ex) {
+                JOptionPane.showMessageDialog(null, "Error al conectarse a base de datos");
+            }
+        }
+        return connection;
+    }
+}
---
+++ src/accesoDatos/productoData.java
@@ -0,0 +1,140 @@
+package accesoDatos;
+
+import entidades.Producto;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author roman
+ */
+public class productoData {
+
+    private Connection con = null;
+
+    public productoData() {
+
+        con = Conexion.getConnection();
+    }
+
+    public void guardarProducto(Producto producto) {
+        String sql = "INSERT INTO producto(nombreProducto, descripcion, precioActual, stock, estado VALUES (?,?,?,?,?)";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, producto.getNombreProducto());
+            ps.setString(2, producto.getDescripcion());
+            ps.setDouble(3, producto.getPrecioActual());
+            ps.setInt(4, producto.getStock());
+            ps.setBoolean(5, producto.isEstado());
+            ps.executeUpdate();
+
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                producto.setIdProducto(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "PRODUCTO GUARDADO CON EXITO");
+
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "NO SE PUDO ACCEDER A LA TABLA PRODUCTO");
+        }
+    }
+
+    public void modificarProducto(Producto producto) {
+
+        String sql = "UPDATE producto SET nombreProducto=?, descripcion=?, precioActual=?, stock=?"
+                + "WHERE idProducto =?";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setString(1, producto.getNombreProducto());
+            ps.setString(2, producto.getDescripcion());
+            ps.setDouble(3, producto.getPrecioActual());
+            ps.setInt(4, producto.getStock());
+            ps.setInt(5, producto.getIdProducto());
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "PRODUCTO MODIFICADO");
+            }
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA PRODUCTO");
+        }
+    }
+
+    public void eliminarProducto(int idProducto) {
+        String sql = "UPDATE producto SET estado = 0 WHERE idProducto= ?";
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idProducto);
+            int exito = ps.executeUpdate();
+            if (exito == 1) {
+                JOptionPane.showMessageDialog(null, "PRODUCTO ELIMINADO");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA PRODUCTO");
+        }
+    }
+
+    public Producto buscarProducto(int idProducto) {
+        String sql = "SELECT idProducto, nombreProducto, descripcion, precioActual, stock FROM producto "
+                + "WHERE idProducto = ? AND estado = 1";
+        Producto producto = null;
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, idProducto);
+            ResultSet rs = ps.executeQuery();
+            if (rs.next()) {
+                producto = new Producto();
+                producto.setIdProducto(rs.getInt("idProducto"));
+                producto.setNombreProducto(rs.getString("nombreProducto"));
+                producto.setDescripcion(rs.getString("descripcion"));
+                producto.setPrecioActual(rs.getDouble("precioActual"));
+                producto.setStock(rs.getInt("stock"));
+                producto.setEstado(true);
+            }
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER LA TABLA PRODUCTO");
+        }
+        return producto;
+    }
+
+    public List<Producto> listarProducto() {
+        String sql = "SELECT idProducto, nombreProducto, descripcion, precioActual, stock FROM producto WHERE estado = 1";
+        ArrayList<Producto> productos = new ArrayList<>();
+        try {
+            PreparedStatement ps = con.prepareStatement(sql);
+
+            ResultSet rs = ps.executeQuery();
+            while (rs.next()) {
+
+                Producto producto = new Producto();
+                producto.setIdProducto(rs.getInt("idProducto"));
+                producto.setNombreProducto(rs.getString("nombreProducto"));
+                producto.setDescripcion(rs.getString("descripcion"));
+                producto.setPrecioActual(rs.getDouble("precioActual"));
+                producto.setStock(rs.getInt("stock"));
+                producto.setEstado(true);
+
+                productos.add(producto);
+            }
+
+            ps.close();
+
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR AL ACCEDER A LA TABLA PRODUCTOS");
+        }
+        return productos;
+
+    }
+}
